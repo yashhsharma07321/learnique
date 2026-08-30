b5604e4b884b1ed3ad29a890a33f6851b8ca603d
Yash Sharma <yashhsharma07798@gmail.com>
2026-08-30 12:30:56 +0530

    Add React import to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import { AuthProvider, useAuth } from './context/AuthContext'
